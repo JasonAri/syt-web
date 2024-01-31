3fa367466d889d1dbed4effd8951b18ff28d87cd
Jason <1058502178@qq.com>
2024-01-31 21:24:25 +0800

    feat(api):add api

---
+++ src/api/home/index.ts
@@ -0,0 +1,11 @@
+// api/home
+import request from "@/utils/request";
+
+// 通过枚举管理首页模块的接口地址
+enum API {
+  HOSPITAL_URL = "/hosp/hospital/",
+}
+
+export const reqHospital = (page: number, limit: number) => {
+  request.get(API.HOSPITAL_URL);
+};
